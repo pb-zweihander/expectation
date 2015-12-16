8b4f7dc345b9215f88e212a1d08da90b4b1424ca
pb-zweihander <sd852456@naver.com>
2015-12-16 20:54:00 +0900

    fixed

--- expectation.py
+++ expectation.py
@@ -5,28 +5,29 @@ import math
 from matplotlib import pyplot
 import numpy
 
-def getExpectation(xbar, s, n, a=1, b=4.5, nbins=100, rndnum=4, pr=False, pl=True, sh=True, plfn=None):
+def getExpectation(xbar, s, n, a=1, b=4.5, nbins=100, pr=False, pl=True, sh=True, plfn=None):
 	arr = []
 	fig = None
 	ax = None
 	if pl:
 		fig = pyplot.figure()
 		ax = fig.add_subplot(1, 1, 1)
-		if sh:
-			pyplot.show()
 	fr = int(a * nbins)
 	ds = int(b * nbins)
 	for za in range(fr, ds, 1):
-		exp = round(integTrap(z, 0, za/nbins, nbins)*2, rndnum+2)
-		mp = round(xbar + ((za/nbins) * (s/math.sqrt(n))), rndnum)
-		mn = round(xbar - ((za/nbins) * (s/math.sqrt(n))), rndnum)
+		exp = integTrap(z, 0, za/nbins, nbins)*2
+		mp = xbar + ((za/nbins) * (s/math.sqrt(n)))
+		mn = xbar - ((za/nbins) * (s/math.sqrt(n)))
 		if pl:
 			ax.plot([mn, mp], [exp, exp], color='orange', linewidth=2)
 		if pr:
-			print("%s - %s : [%s, %s]" % (round(za/nbins, rndnum), exp, mn, mp))
-		arr.append((za/nbins, round(exp*100, rndnum), mn, mp))
+			print("%s - %s : [%s, %s]" % (za/nbins, exp, mn, mp))
+		arr.append((za/nbins, exp, mn, mp))
 	if pl and plfn:
-		pyplot.savefig(filename)
+		pyplot.savefig(plfn)
+	if pl and sh:
+		pyplot.show()
+	print("Done!")
 	if pl:
 		return {'array':arr, 'figure':fig, 'axis':ax}
 	else:
--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 
 from expectation import *
 
-dic = getExpectation(xbar=75, s=4, n=64, a=1, b=4.5, nbins=1000, rndnum=5, pr=True, pl=True, sh=True, plfn='result.png')
+dic = getExpectation(xbar=0, s=1, n=100, a=0.01, b=5, nbins=500, pr=True, pl=True, sh=True, plfn='result.png')
 
