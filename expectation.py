fbb8f12d502c6281902ac770e88557fb104a3b24
pb-zweihander <sd852456@naver.com>
2015-12-15 20:37:32 -0500

    move plot to expectation.py

--- expectation.py
+++ expectation.py
@@ -2,15 +2,33 @@
 from __future__ import division
 from integration import *
 import math
+from matplotlib import pyplot
+import numpy
 
-def getExpectation(xbar, s, n, a=1, b=4.5, nbins=100, rndnum=4):
+def getExpectation(xbar, s, n, a=1, b=4.5, nbins=100, rndnum=4, pr=False, pl=True, sh=True, plfn=None):
 	arr = []
+	fig = None
+	ax = None
+	if pl:
+		fig = pyplot.figure()
+		ax = fig.add_subplot(1, 1, 1)
+		if sh:
+			pyplot.show()
 	fr = int(a * nbins)
 	ds = int(b * nbins)
 	for za in range(fr, ds, 1):
 		exp = round(integTrap(0, za/nbins, z, 1000)*2, rndnum+2)
 		mp = round(xbar + ((za/nbins) * (s/math.sqrt(n))), rndnum)
 		mn = round(xbar - ((za/nbins) * (s/math.sqrt(n))), rndnum)
+		if pl:
+			ax.plot([mn, mp], [exp, exp], color='orange', linewidth=2)
+		if pr:
+			print("%s - %s : [%s, %s]" % (round(za/nbins, rndnum), exp, mn, mp))
 		arr.append((za/nbins, round(exp*100, rndnum), mn, mp))
-	return arr
+	if pl and plfn:
+		pyplot.savefig(filename)
+	if pl:
+		return {'array':arr, 'figure':fig, 'axis':ax}
+	else:
+		return arr
 
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python
 
 from expectation import *
-from matplotlib import pyplot
-import numpy
 
-arr = getExpectation(75, 4, 64, 1, 4.5, 1000, 5)
-
-fig = pyplot.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-for (za, exp, mn, mp) in arr:
-	ax.plot([mn, mp], [exp, exp], color='orange', linewidth=3)
-
-pyplot.savefig('result.png')
-#pyplot.show()
+dic = getExpectation(xbar=75, s=4, n=64, a=1, b=4.5, nbins=1000, rndnum=5, pr=True, pl=True, sh=True, plfn='result.png')
 
