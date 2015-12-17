41a292128085e1a4a2f1ac73e13780813f461315
pb-zweihander <sd852456@naver.com>
2015-12-16 22:29:09 -0500

    move plot to main, add reverseExpectation

--- expectation.py
+++ expectation.py
@@ -2,34 +2,23 @@
 from __future__ import division
 from integration import *
 import math
-from matplotlib import pyplot
-import numpy
 
-def getExpectation(xbar, s, n, a=1, b=4.5, nbins=100, pr=False, pl=True, sh=True, plfn=None):
-	arr = []
-	fig = None
-	ax = None
-	if pl:
-		fig = pyplot.figure()
-		ax = fig.add_subplot(1, 1, 1)
+def getExpectation(xbar=0, s=1, n=100, a=1, b=4.5, nbins=100, pr=False):
+	arr =[]
 	fr = int(a * nbins)
 	ds = int(b * nbins)
 	for za in range(fr, ds, 1):
 		exp = integTrap(z, 0, za/nbins, nbins)*2
 		mp = xbar + ((za/nbins) * (s/math.sqrt(n)))
 		mn = xbar - ((za/nbins) * (s/math.sqrt(n)))
-		if pl:
-			ax.plot([mn, mp], [exp, exp], color='orange', linewidth=2)
 		if pr:
 			print("%s - %s : [%s, %s]" % (za/nbins, exp, mn, mp))
 		arr.append((za/nbins, exp, mn, mp))
-	if pl and plfn:
-		pyplot.savefig(plfn)
-	if pl and sh:
-		pyplot.show()
 	print("Done!")
-	if pl:
-		return {'array':arr, 'figure':fig, 'axis':ax}
-	else:
-		return arr
+	return arr
+
+def reverseExpectation(mp, xbar=0, s=1, n=100, nbins=100):
+	za = (mp - xbar) * math.sqrt(n) / s
+	exp = integTrap(z, 0, za, nbins)
+	return exp
 
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 
 from expectation import *
+from matplotlib import pyplot
+import numpy
 
-dic = getExpectation(xbar=0, s=1, n=100, a=0.01, b=5, nbins=500, pr=True, pl=True, sh=True, plfn='result.png')
+arr = getExpectation(xbar=0, s=1, n=100, a=0.01, b=5, nbins=500, pr=True, pl=True, sh=True, plfn='result.png')
+
+fig = pyplot.plot()
+ax = fig.add_subplot(1, 1, 1)
+
+for (za, exp, mn, mp) in arr:
+	ax.plot([mn, mp], [exp, exp], color='orange', linewidth=2)
+
+pyplot.savefig("result.png")
+pyplot.show()
 
